7aa011471d7c1b8a168b63cefb110a24b7ca9b82
Danilo Cicerone <cydside@gmail.com>
2018-01-09 14:35:23 +0100

    Update dayfolders.go
    Added day's name and month's name flag selection.

--- dayfolders.go
+++ dayfolders.go
@@ -72,42 +72,42 @@ const (
 var DayNames = map[Locale]map[Format]map[time.Weekday]string{
 	"en_US": {
 		short: {
-			Sunday: "Sun",
-			Monday: "Mon",
-			Tuesday: "Tue",
+			Sunday:    "Sun",
+			Monday:    "Mon",
+			Tuesday:   "Tue",
 			Wednesday: "Wed",
-			Thursday: "Thu",
-			Friday: "Fri",
-			Saturday: "Sat",
+			Thursday:  "Thu",
+			Friday:    "Fri",
+			Saturday:  "Sat",
 		},
 		long: {
-			Sunday: "Sunday",
-			Monday: "Monday",
-			Tuesday: "Tuesday",
+			Sunday:    "Sunday",
+			Monday:    "Monday",
+			Tuesday:   "Tuesday",
 			Wednesday: "Wednesday",
-			Thursday: "Thursday",
-			Friday: "Friday",
-			Saturday: "Saturday",
+			Thursday:  "Thursday",
+			Friday:    "Friday",
+			Saturday:  "Saturday",
 		},
 	},
 	"it_IT": {
 		short: {
-			Sunday: "Dom",
-			Monday: "Lun",
-			Tuesday: "Mar",
+			Sunday:    "Dom",
+			Monday:    "Lun",
+			Tuesday:   "Mar",
 			Wednesday: "Mer",
-			Thursday: "Gio",
-			Friday: "Ven",
-			Saturday: "Sab",
+			Thursday:  "Gio",
+			Friday:    "Ven",
+			Saturday:  "Sab",
 		},
 		long: {
-			Sunday: "Domenica",
-			Monday: "Lunedì",
-			Tuesday: "Martedì",
+			Sunday:    "Domenica",
+			Monday:    "Lunedì",
+			Tuesday:   "Martedì",
 			Wednesday: "Mercoledì",
-			Thursday: "Giovedì",
-			Friday: "Venerdì",
-			Saturday: "Sabato",
+			Thursday:  "Giovedì",
+			Friday:    "Venerdì",
+			Saturday:  "Sabato",
 		},
 	},
 }
@@ -115,69 +115,69 @@ var DayNames = map[Locale]map[Format]map[time.Weekday]string{
 var MonthNames = map[Locale]map[Format]map[time.Month]string{
 	"en_US": {
 		short: {
-			January : "Jan",
-			February: "Feb",
-			March: "Mar",
-			April: "Apr",
-			May: "May",
-			June: "Jun",
-			July: "Jul",
-			August: "Aug",
+			January:   "Jan",
+			February:  "Feb",
+			March:     "Mar",
+			April:     "Apr",
+			May:       "May",
+			June:      "Jun",
+			July:      "Jul",
+			August:    "Aug",
 			September: "Sep",
-			October: "Oct",
-			November: "Nov",
-			December: "Dec",
+			October:   "Oct",
+			November:  "Nov",
+			December:  "Dec",
 		},
 		long: {
-			January : "January",
-			February: "February",
-			March: "March",
-			April: "April",
-			May: "May",
-			June: "June",
-			July: "July",
-			August: "August",
+			January:   "January",
+			February:  "February",
+			March:     "March",
+			April:     "April",
+			May:       "May",
+			June:      "June",
+			July:      "July",
+			August:    "August",
 			September: "September",
-			October: "October",
-			November: "November",
-			December: "December",
+			October:   "October",
+			November:  "November",
+			December:  "December",
 		},
 	},
 	"it_IT": {
 		short: {
-			January : "Gen",
-			February: "Feb",
-			March: "Mar",
-			April: "Apr",
-			May: "Mag",
-			June: "Giu",
-			July: "Lug",
-			August: "Aug",
+			January:   "Gen",
+			February:  "Feb",
+			March:     "Mar",
+			April:     "Apr",
+			May:       "Mag",
+			June:      "Giu",
+			July:      "Lug",
+			August:    "Aug",
 			September: "Set",
-			October: "Ott",
-			November: "Nov",
-			December: "Dic",
+			October:   "Ott",
+			November:  "Nov",
+			December:  "Dic",
 		},
 		long: {
-			January : "Gennaio",
-			February: "Febbraio",
-			March: "Marzo",
-			April: "Aprile",
-			May: "Maggio",
-			June: "Giugno",
-			July: "Luglio",
-			August: "Agosto",
+			January:   "Gennaio",
+			February:  "Febbraio",
+			March:     "Marzo",
+			April:     "Aprile",
+			May:       "Maggio",
+			June:      "Giugno",
+			July:      "Luglio",
+			August:    "Agosto",
 			September: "Settembre",
-			October: "Ottobre",
-			November: "Novembre",
-			December: "Dicembre",
+			October:   "Ottobre",
+			November:  "Novembre",
+			December:  "Dicembre",
 		},
 	},
 }
 
 var yearPtr, fromPtr, toPtr, pathPtr string
-var onefPtr, subfPtr, dowPtr, doyPtr, verPtr bool
-var daysPtr int
+var onefPtr, subfPtr, doyPtr, verPtr bool
+var daysPtr, dowPtr, domPtr int
 var dateStart, dateEnd time.Time
 var out io.Writer = ioutil.Discard
 
@@ -193,8 +193,8 @@ func main() {
 			"[-sub] [-one] [-dow] [-doy] [-ver]\n\nOptions:\n")
 		flag.PrintDefaults()
 		fmt.Printf("\nCopyright (C) 2017-2018 Danilo Cicerone.\n" +
-			"This is free software; see the source for copying conditions. "+
-			"There is NO warranty; not even for MERCHANTABILITY or FITNESS "+
+			"This is free software; see the source for copying conditions. " +
+			"There is NO warranty; not even for MERCHANTABILITY or FITNESS " +
 			"FOR A PARTICULAR PURPOSE.\n")
 	}
 
@@ -219,7 +219,12 @@ func main() {
 	flag.IntVar(&daysPtr, "days", 0, "Number of the days(1 to 366) to add or "+
 		"subtract(only in case of option '-to').")
 
-	flag.BoolVar(&dowPtr, "dow", false, "Adds short names of the day.")
+	flag.IntVar(&dowPtr, "dow", 0, "0=No day's name added(default), 1=Adds "+
+		"short names of the day, 2=Adds names of the day.")
+
+	flag.IntVar(&domPtr, "dom", 0, "0=No month's name added(default), 1=Adds "+
+		"short names of the month, 2=Adds names of the month. Doesn't affect"+
+		" 'one' option.")
 
 	flag.BoolVar(&doyPtr, "doy", false, "Adds day of the year.")
 
@@ -322,7 +327,6 @@ func validateFlags() error {
 //------------------------------------------------------------------------------
 
 func validatePeriod() error {
-
 	var errDateFormat error = errors.New("Unexpected date format. Goodbye!")
 
 	if yearPtr != "" {
@@ -380,46 +384,66 @@ func validatePeriod() error {
 
 //------------------------------------------------------------------------------
 
-func createFolders() error {
+func deleteEmpty(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
+}
 
+//------------------------------------------------------------------------------
+
+func createFolders() error {
 	dayOfYear := ""
 	dateCursor := dateStart
 	yearFormat := "2006"
 	monthFormat := "01"
 	dayFormat := "02"
-	shortDayName := ""
-	shortMonthName := ""
+	dayName := ""
+	monthName := ""
 
 	for dateCursor.Before(dateEnd) || dateCursor.Equal(dateEnd) {
 		if doyPtr {
 			dayOfYear = fmt.Sprintf("%03d", dateCursor.YearDay())
 		}
 
-		if dowPtr {
-			shortDayName = fmt.Sprintf("%s", DayNames["it_IT"][short][dateCursor.Weekday()])
-			shortMonthName = fmt.Sprintf("%s", MonthNames["it_IT"][long][dateCursor.Month()])
+		switch dowPtr {
+		case 1:
+			dayName = fmt.Sprintf("%s", DayNames["it_IT"][short][dateCursor.Weekday()])
+		case 2:
+			dayName = fmt.Sprintf("%s", DayNames["it_IT"][long][dateCursor.Weekday()])
+		}
+
+		switch domPtr {
+		case 1:
+			monthName = fmt.Sprintf("%s", MonthNames["it_IT"][short][dateCursor.Month()])
+		case 2:
+			monthName = fmt.Sprintf("%s", MonthNames["it_IT"][long][dateCursor.Month()])
 		}
 
 		if (!onefPtr && !subfPtr) || subfPtr {
-			s := []string{dayOfYear, dateCursor.Format(dayFormat), shortDayName}
-			m := []string{dateCursor.Format(monthFormat), shortMonthName}
+			s := []string{dayOfYear, dateCursor.Format(dayFormat), dayName}
+			m := []string{dateCursor.Format(monthFormat), monthName}
 			err := os.MkdirAll(
 				filepath.Join(
 					pathPtr,
 					dateCursor.Format(yearFormat),
-					strings.Join(m, "_"),
-					strings.Join(s, "_"),
+					strings.Join(deleteEmpty(m), "_"),
+					strings.Join(deleteEmpty(s), "_"),
 				), os.ModePerm)
 
 			if err != nil {
 				return err
 			}
 		} else {
-			s := []string{dateCursor.Format("2006-01-02"), dayOfYear, shortDayName}
+			s := []string{dateCursor.Format("2006-01-02"), dayOfYear, dayName}
 			err := os.MkdirAll(filepath.Join(
 				pathPtr,
 				dateCursor.Format(yearFormat),
-				strings.Join(s, "_"),
+				strings.Join(deleteEmpty(s), "_"),
 			), os.ModePerm)
 
 			if err != nil {
